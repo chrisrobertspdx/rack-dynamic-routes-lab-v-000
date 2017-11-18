653ac37b67889156b6b5f14d83073dff22522c5c
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-18 22:44:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/item/)
-      item_name = req.path.split("/item/").last
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
       if my_item = Item.find{|i| i.name == item_name}
         resp.write "#{my_item.name} cost #{my_item.price}"
         resp.status = 200
