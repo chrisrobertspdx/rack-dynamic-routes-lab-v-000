68ffebf693ffa2b1fa0f2ff28a2c2895028270d2
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-18 21:59:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/item/)
       item_name = req.path.split("/item/").last
-      item = Item.items.find(|i| i.name == item_name)
+      item = Item.items.find{|i| i.name == item_name}
       if !!item
         resp.write "#{item.name} cost #{item/price}"
       else
