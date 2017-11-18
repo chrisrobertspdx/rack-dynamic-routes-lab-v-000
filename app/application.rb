0f4b5e9a8c9d7dad7077130292e6bd3b8dd4d6a2
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-18 22:12:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       if !!item
         resp.write "#{item.name} cost #{item.price}"
       else
-        resp.write "Route not found"
+        resp.write "Item not found"
         resp.status = 404
       end
     else
