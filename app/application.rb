1a7753aae81820ee3a02019f555141f624032229
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-18 22:20:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/item/)
       item_name = req.path.split("/item/").last
       my_item = Item.items.find{|i| i.name == item_name}
-      if !!my_item
+      if my_item !== nil
         resp.write "#{my_item.name} cost #{my_item.price}"
         resp.status = 200
       else
